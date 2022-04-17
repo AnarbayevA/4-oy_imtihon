74b82518207044f1c394493fbf6bba1a22ec8639
AnarbayevA <azamat2000.com.com@gmail.com>
2022-04-17 08:30:43 +0300

    UI

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Get HTML element
+
+let elSearchInput = document.querySelector("#search_input");
+
+
+let elResultsNumber = document.querySelector(".hero__result-number");
+let elOrderByNews = document.querySelector(".hero__btn");
+
+let elBookmarkWrapper = document.querySelector(".bookmark__books");
+let elBookmarkTemplate = document.querySelector("#bookmark-template").content;
+
+let elBooksMarketTemplateWrapper = document.querySelector(".books-market__top");
+let elBooksMarketTemplate = document.querySelector("#books-market-template").content;
+
+let elModalWrapper = document.querySelector(".modal-header");
+let elMoreInformationTemplate = document.querySelector("#more-information-template").content;
+
+// function renderBooks()
+
+
+
+// https://www.googleapis.com/books/v1/volumes?q=$%7Bshu_yerga_yoziladi%7D
+
